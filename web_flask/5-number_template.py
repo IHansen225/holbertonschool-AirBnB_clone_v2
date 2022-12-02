2c0da260bea0084539e34b8424f0440421659379
Ivan <ihk225@gmail.com>
2022-12-02 18:36:09 -0300

    Ex5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -43,9 +43,7 @@ def number_route(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template_route(n):
     """ Return function for the corresponding route """
-    return render_template('5-number.html', body="""
-    <h1>Number: {}</h1>
-    """.format(n))
+    return render_template('5-number.html', number=n)
 
 
 if __name__ == '__main__':
