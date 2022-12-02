8debc7d210664f0b1a0bef2216f55c40d135874e
Ivan <ihk225@gmail.com>
2022-12-02 15:36:30 -0300

    Readme

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
     """ Return function for the corresponding route """
     return 'Hello HBNB!'
 
+
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run(debug=True)
