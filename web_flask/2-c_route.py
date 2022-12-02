ac7ed0f4834c2c1d381eb05245c8112a895bfaa7
Ivan <ihk225@gmail.com>
2022-12-02 17:25:35 -0300

    Ex2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+    Flask test module
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Return function for the corresponding route """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return function for the corresponding route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_route(text):
+    """ Return function for the corresponding route """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+if __name__ == '__main__':
+    app.run(debug=True)
