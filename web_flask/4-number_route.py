a0d5539aa9257dbbb65b3b19a0005f59f05e70e9
Ivan <ihk225@gmail.com>
2022-12-02 18:01:56 -0300

    Ex3

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+    Flask test module
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def index():
+    """ Return function for the corresponding route """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Return function for the corresponding route """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def text_route(text):
+    """ Return function for the corresponding route """
+    text = text.replace('_', ' ')
+    return 'C {}'.format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_route(text='is cool'):
+    """ Return function for the corresponding route """
+    text = text.replace('_', ' ')
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number_route(n):
+    """ Return function for the corresponding route """
+    return n
+
+if __name__ == '__main__':
+    app.run(debug=True)
